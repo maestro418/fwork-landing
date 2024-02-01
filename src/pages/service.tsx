0112407b6986195bbee6ad202ba89b75b088aa83
unknown <mrsteevtvw418@gmail.com>
2024-02-01 16:39:33 +0000

    feat: add 404 page

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link, useParams } from "react-router-dom";
 
@@ -13,6 +13,7 @@ import AskQuestion from "../components/ask-question";
 import BlogCard from "../components/blog-card";
 import OutSourcing from "../components/outsourcing";
 import { StyledButton } from "../components/button";
+import E404 from "./e404";
 
 interface dataType {
     title: string
@@ -59,13 +60,17 @@ const Service = () => {
     const [status, setStatus] = React.useState({} as dataType);
     const [smallScreen, setSmallScreen] = React.useState(false);
     const [activeIndex, setActiveIndex] = React.useState<number | null>(null);
-    const [imgPath, setImgPath] = React.useState<string | "">()
+    const [imgPath, setImgPath] = React.useState<string | "">();
+    const [isValidId, setIsValidId] = useState(true)
     const onAccordian = (idx: number) => {
         setActiveIndex((prevIndex) => (prevIndex === idx ? null : idx));
     };
 
     React.useEffect(() => {
         const d = data.find((i) => (i.title === id))
+        if (!d) {
+            setIsValidId(false)
+        }
         setImgPath(d?.title)
         const _data = d?.item;
         if (!!_data) {
@@ -82,88 +87,95 @@ const Service = () => {
     }, []);
 
     return (
-        <Layout>
-            <StyledService>
-                <IntroBanner title={status.title} desc={status.desc} bgImg={`/img/bg/${imgPath}.png`} />
-                <section>
-                    <div className="container">
-                        <div className="text-header">
-                            <h1>{status.service?.title}</h1>
-                            <div className="m-0 p-0">{status.service?.desc}</div>
-                        </div>
-                        <div className="row">
-                            {status.service?.item.map((i, k) => (
-                                <div className="col-md-6 mt mb" key={k}>
-                                    <OutSourcing img={i.img} title={i.title} desc={i.desc} />
+        <>
+            {!!isValidId && (
+                <Layout>
+                    <StyledService>
+                        <IntroBanner title={status.title} desc={status.desc} bgImg={`/img/bg/${imgPath}.png`} />
+                        <section>
+                            <div className="container">
+                                <div className="text-header">
+                                    <h1>{status.service?.title}</h1>
+                                    <div className="m-0 p-0">{status.service?.desc}</div>
                                 </div>
-                            ))}
-                        </div>
-                    </div>
-                </section>
-                <section className="case-content">
-                    <div className="container">
-                        <label className="h1">{status.project?.title}</label>
-                        <div className="row">
-                            {status.project?.item.map((i, k) => (
-                                <div className="col-md-4" key={k}>
-                                    <CaseCard img={`service/${i.img}`} title={i.title} tech={i.tech} team={i.team} smallScreen={smallScreen} />
+                                <div className="row">
+                                    {status.service?.item.map((i, k) => (
+                                        <div className="col-md-6 mt mb" key={k}>
+                                            <OutSourcing img={i.img} title={i.title} desc={i.desc} />
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="d center gap mt-1 mb-2">
-                        <Link to='https://fwork.io/freelancers'><StyledButton>Hire Top-notch Developer
-                        </StyledButton></Link>
-                        <Link to="https://fwork.io/blogs">
-                            <StyledButton className="d middle" >
-                                <div className="pr">See More Case Studies</div>
-                                <img src="/img/icon/right-arrow.svg" width={16} alt="" />
-                            </StyledButton>
-                        </Link>
-                    </div>
-                </section>
-                <section className="container">
-                    <div className="row">
-                        <div className="col-md-4 text-header">
-                            <label className="h1">{status.faq?.title}</label>
-                            <div>Everything you need to know about DevTeam.Space. Can’t find the answer you’re looking for?</div>
-                        </div>
-                        <div className="col-md-8 pt-2">
-                            {status.faq?.item.map((i, k) => (
-                                <AskQuestion
-                                    title={i.title}
-                                    desc={i.desc}
-                                    isOpen={activeIndex === k}
-                                    onAccordian={() => onAccordian(k)}
-                                    key={k}
-                                />
-                            ))}
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <div className="container">
-                        <div className="text-header">
-                            <label className="h1">{status.blog?.title}</label>
-                            <div>{status.blog?.desc}</div>
-                        </div>
-                        <div className="row">
-                            {status.blog?.item.map((i, k) => (
-                                <div key={k} className="col-sm-4">
-                                    <BlogCard banner={i.banner} title={i.title} desc={i.desc} img={`service/${i.img}`} link={i.banner} />
+                            </div>
+                        </section>
+                        <section className="case-content">
+                            <div className="container">
+                                <label className="h1">{status.project?.title}</label>
+                                <div className="row">
+                                    {status.project?.item.map((i, k) => (
+                                        <div className="col-md-4" key={k}>
+                                            <CaseCard img={`service/${i.img}`} title={i.title} tech={i.tech} team={i.team} smallScreen={smallScreen} />
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
-                        </div>
-                    </div>
-                </section>
-                <section className="mb-5">
-                    <div className="container">
-                        {status.article && <Markdown text={status.article} />}
-                    </div>
-                </section>
+                            </div>
+                            <div className="d center gap mt-1 mb-2">
+                                <Link to='https://fwork.io/freelancers'><StyledButton>Hire Top-notch Developer
+                                </StyledButton></Link>
+                                <Link to="https://fwork.io/blogs">
+                                    <StyledButton className="d middle" >
+                                        <div className="pr">See More Case Studies</div>
+                                        <img src="/img/icon/right-arrow.svg" width={16} alt="" />
+                                    </StyledButton>
+                                </Link>
+                            </div>
+                        </section>
+                        <section className="container">
+                            <div className="row">
+                                <div className="col-md-4 text-header">
+                                    <label className="h1">{status.faq?.title}</label>
+                                    <div>Everything you need to know about DevTeam.Space. Can’t find the answer you’re looking for?</div>
+                                </div>
+                                <div className="col-md-8 pt-2">
+                                    {status.faq?.item.map((i, k) => (
+                                        <AskQuestion
+                                            title={i.title}
+                                            desc={i.desc}
+                                            isOpen={activeIndex === k}
+                                            onAccordian={() => onAccordian(k)}
+                                            key={k}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                        </section>
+                        <section>
+                            <div className="container">
+                                <div className="text-header">
+                                    <label className="h1">{status.blog?.title}</label>
+                                    <div>{status.blog?.desc}</div>
+                                </div>
+                                <div className="row">
+                                    {status.blog?.item.map((i, k) => (
+                                        <div key={k} className="col-sm-4">
+                                            <BlogCard banner={i.banner} title={i.title} desc={i.desc} img={`service/${i.img}`} link={i.banner} />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </section>
+                        <section className="mb-5">
+                            <div className="container">
+                                {status.article && <Markdown text={status.article} />}
+                            </div>
+                        </section>
+                    </StyledService>
+                </Layout>
+            )}
+            {!isValidId && (
+                <E404 />
+            )}
+        </>
 
-            </StyledService>
-        </Layout>
     )
 }
 
